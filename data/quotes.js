499c1d12db297952bd47aebbb80674a5671951a0
Tucker Hoog <tuckerhoog@tutanota.com>
2021-05-19 17:25:54 -0400

    added data

---
+++ data/quotes.js
@@ -0,0 +1,386 @@
+const quotes = {
+  'page': 1,
+  'last_page': false,
+  'quotes': [
+    {
+      'id': 39865,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'medical'
+      ],
+      'url': 'https://favqs.com/quotes/jack-welch/39865-what-s-import-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Jack Welch',
+      'author_permalink': 'jack-welch',
+      'body': 'What\'s important at the grocery store is just as important in engines or medical systems. If the customer isn\'t satisfied, if the stuff is getting stale, if the shelf isn\'t right, or if the offerings aren\'t right, it\'s the same thing. You manage it like a small organization. You don\'t get hung up on zeros.'
+    },
+    {
+      'id': 21359,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'fear',
+        'home',
+        'motivational'
+      ],
+      'url': 'https://favqs.com/quotes/dale-carnegie/21359-if-you-want-t-',
+      'favorites_count': 5,
+      'upvotes_count': 0,
+      'downvotes_count': 0,
+      'author': 'Dale Carnegie',
+      'author_permalink': 'dale-carnegie',
+      'body': 'If you want to conquer fear, don\'t sit home and think about it. Go out and get busy.'
+    },
+    {
+      'id': 646,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'injustice',
+        'justice',
+        'mlk',
+        'famous'
+      ],
+      'url': 'https://favqs.com/quotes/martin-luther-king-jr/646-injustice-anywh-',
+      'favorites_count': 3,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Martin Luther King, Jr.',
+      'author_permalink': 'martin-luther-king-jr',
+      'body': 'Injustice anywhere is a threat to justice everywhere.'
+    },
+    {
+      'id': 27596,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'good'
+      ],
+      'url': 'https://favqs.com/quotes/nelson-mandela/27596-nonviolence-i-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Nelson Mandela',
+      'author_permalink': 'nelson-mandela',
+      'body': 'Nonviolence is a good policy when the conditions permit.'
+    },
+    {
+      'id': 29019,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'great',
+        'intelligence'
+      ],
+      'url': 'https://favqs.com/quotes/ralph-waldo-emerson/29019-character-is--',
+      'favorites_count': 1,
+      'upvotes_count': 2,
+      'downvotes_count': 0,
+      'author': 'Ralph Waldo Emerson',
+      'author_permalink': 'ralph-waldo-emerson',
+      'body': 'Character is higher than intellect. A great soul will be strong to live as well as think.'
+    },
+    {
+      'id': 60502,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'women'
+      ],
+      'url': 'https://favqs.com/quotes/stephen-hawking/60502-women-they-ar-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Stephen Hawking',
+      'author_permalink': 'stephen-hawking',
+      'body': 'Women. They are a complete mystery.'
+    },
+    {
+      'id': 17362,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'experience',
+        'good'
+      ],
+      'url': 'https://favqs.com/quotes/will-rogers/17362-good-judgment-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Will Rogers',
+      'author_permalink': 'will-rogers',
+      'body': 'Good judgment comes from experience, and a lot of that comes from bad judgment.'
+    },
+    {
+      'id': 8023,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'business'
+      ],
+      'url': 'https://favqs.com/quotes/will-rogers/8023-we-don-t-seem--',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Will Rogers',
+      'author_permalink': 'will-rogers',
+      'body': 'We don\'t seem to be able to check crime, so why not legalize it and then tax it out of business?'
+    },
+    {
+      'id': 4121,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'architecture',
+        'music',
+        'work'
+      ],
+      'url': 'https://favqs.com/quotes/samuel-butler/4121-every-man-s-wo-',
+      'favorites_count': 2,
+      'upvotes_count': 2,
+      'downvotes_count': 0,
+      'author': 'Samuel Butler',
+      'author_permalink': 'samuel-butler',
+      'body': 'Every man\'s work, whether it be literature, or music or pictures or architecture or anything else, is always a portrait of himself.'
+    },
+    {
+      'id': 63265,
+      'dialogue': false,
+      'private': false,
+      'tags': [],
+      'url': 'https://favqs.com/quotes/lalit-goyal/63265-never-let-you-',
+      'favorites_count': 1,
+      'upvotes_count': 0,
+      'downvotes_count': 0,
+      'author': 'Lalit Goyal',
+      'author_permalink': 'lalit-goyal',
+      'body': 'Never let your schooling interfere with your education!!!'
+    },
+    {
+      'id': 38680,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'love'
+      ],
+      'url': 'https://favqs.com/quotes/oscar-wilde/38680-when-a-man-ha-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Oscar Wilde',
+      'author_permalink': 'oscar-wilde',
+      'body': 'When a man has once loved a woman he will do anything for her except continue to love her.'
+    },
+    {
+      'id': 30380,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'happiness'
+      ],
+      'url': 'https://favqs.com/quotes/glen-hansard/30380-sadness-is-a--',
+      'favorites_count': 1,
+      'upvotes_count': 0,
+      'downvotes_count': 0,
+      'author': 'Glen Hansard',
+      'author_permalink': 'glen-hansard',
+      'body': 'Sadness is a very interesting idea, this idea of sadness being some kind of default setting that artists will go into. And then I started thinking about this idea of sadness and happiness, and the idea that sadness is very loud, and happiness is quiet.'
+    },
+    {
+      'id': 31398,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'history'
+      ],
+      'url': 'https://favqs.com/quotes/percy-bysshe-shelley/31398-history-is-a--',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Percy Bysshe Shelley',
+      'author_permalink': 'percy-bysshe-shelley',
+      'body': 'History is a cyclic poem written by time upon the memories of man.'
+    },
+    {
+      'id': 63688,
+      'dialogue': false,
+      'private': false,
+      'tags': [],
+      'url': 'https://favqs.com/quotes/harriet-tubman/63688-every-great-d-',
+      'favorites_count': 2,
+      'upvotes_count': 0,
+      'downvotes_count': 0,
+      'author': 'Harriet Tubman',
+      'author_permalink': 'harriet-tubman',
+      'body': 'Every great dream begins with a dreamer. Always remember, you have within you the strength, patience, and passion to reach for the stars and change the world.'
+    },
+    {
+      'id': 64295,
+      'dialogue': false,
+      'private': false,
+      'tags': [],
+      'url': 'https://favqs.com/quotes/adina/64295-this-is-my-qu-',
+      'favorites_count': 1,
+      'upvotes_count': 0,
+      'downvotes_count': 0,
+      'author': 'Adina',
+      'author_permalink': 'adina',
+      'body': 'This is my quote for performance testing'
+    },
+    {
+      'id': 62416,
+      'dialogue': false,
+      'source': 'Star Wars Episode V: The Empire Strikes Back',
+      'context': 'Trying to turn Luke to the dark side after having just cut his hand off with a lightsaber',
+      'private': false,
+      'tags': [],
+      'url': 'https://favqs.com/quotes/darth-vader/62416-no-i-am-your--',
+      'favorites_count': 1,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Darth Vader',
+      'author_permalink': 'darth-vader',
+      'body': 'No, I am your father.'
+    },
+    {
+      'id': 13810,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'death',
+        'great'
+      ],
+      'url': 'https://favqs.com/quotes/george-eliot/13810-when-death-th-',
+      'favorites_count': 1,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'George Eliot',
+      'author_permalink': 'george-eliot',
+      'body': 'When death, the great reconciler, has come, it is never our tenderness that we repent of, but our severity.'
+    },
+    {
+      'id': 9783,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'change'
+      ],
+      'url': 'https://favqs.com/quotes/leonardo-da-vinci/9783-he-who-is-fixe-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Leonardo da Vinci',
+      'author_permalink': 'leonardo-da-vinci',
+      'body': 'He who is fixed to a star does not change his mind.'
+    },
+    {
+      'id': 57275,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'time'
+      ],
+      'url': 'https://favqs.com/quotes/bertrand-russell/57275-a-truer-image-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Bertrand Russell',
+      'author_permalink': 'bertrand-russell',
+      'body': 'A truer image of the world, I think, is obtained by picturing things as entering into the stream of time from an eternal world outside, than from a view which regards time as the devouring tyrant of all that is.'
+    },
+    {
+      'id': 4646,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'art'
+      ],
+      'url': 'https://favqs.com/quotes/pablo-picasso/4646-there-is-no-ab-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Pablo Picasso',
+      'author_permalink': 'pablo-picasso',
+      'body': 'There is no abstract art. You must always start with something. Afterward you can remove all traces of reality.'
+    },
+    {
+      'id': 13626,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'death',
+        'fear'
+      ],
+      'url': 'https://favqs.com/quotes/tupac-shakur/13626-i-don-t-have--',
+      'favorites_count': 2,
+      'upvotes_count': 0,
+      'downvotes_count': 0,
+      'author': 'Tupac Shakur',
+      'author_permalink': 'tupac-shakur',
+      'body': 'I don\'t have no fear of death. My only fear is coming back reincarnated.'
+    },
+    {
+      'id': 23412,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'freedom'
+      ],
+      'url': 'https://favqs.com/quotes/albert-camus/23412-freedom-is-no-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Albert Camus',
+      'author_permalink': 'albert-camus',
+      'body': 'Freedom is nothing but a chance to be better.'
+    },
+    {
+      'id': 4110,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'architecture'
+      ],
+      'url': 'https://favqs.com/quotes/henry-david-thoreau/4110-they-can-do-wi-',
+      'favorites_count': 1,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Henry David Thoreau',
+      'author_permalink': 'henry-david-thoreau',
+      'body': 'They can do without architecture who have no olives nor wines in the cellar.'
+    },
+    {
+      'id': 63344,
+      'dialogue': false,
+      'private': false,
+      'tags': [],
+      'url': 'https://favqs.com/quotes/anonymous/63344-never-let-you-',
+      'favorites_count': 1,
+      'upvotes_count': 0,
+      'downvotes_count': 0,
+      'author': 'Anonymous',
+      'author_permalink': 'anonymous',
+      'body': 'Never let your schooling interfere with your education.'
+    },
+    {
+      'id': 39067,
+      'dialogue': false,
+      'private': false,
+      'tags': [
+        'marriage'
+      ],
+      'url': 'https://favqs.com/quotes/michel-de-montaigne/39067-marriage-is-l-',
+      'favorites_count': 0,
+      'upvotes_count': 1,
+      'downvotes_count': 0,
+      'author': 'Michel de Montaigne',
+      'author_permalink': 'michel-de-montaigne',
+      'body': 'Marriage is like a cage; one sees the birds outside desperate to get in, and those inside equally desperate to get out.'
+    }
+  ]
+};
+
+export default quotes;
